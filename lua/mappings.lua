d12aeacffeb79371ba0d52fe03a4baaf9bfe9234
Brendan Davidson <binary141@gmail.com>
2026-05-25 09:14:00 -0600

    Add go to type definition

--- lua/mappings.lua
+++ lua/mappings.lua
@@ -29,6 +29,7 @@ vim.keymap.set('n', '<S-Tab>', '<Cmd>BufferPrevious<CR>', {})
 local opts = { buffer = bufnr, noremap = true, silent = true }
 
 vim.keymap.set('n', 'gd', vim.lsp.buf.definition, opts)
+vim.keymap.set('n', 'gtd', vim.lsp.buf.type_definition, opts)
 vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, opts)
 vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, opts)
 vim.keymap.set('n', 'gr', vim.lsp.buf.references, opts)
